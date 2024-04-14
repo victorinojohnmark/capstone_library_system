5358665b9e2e92bb82a268976586e808428915cc
victorinojohnmark <victorinojohnmark@gmail.com>
2024-04-14 21:56:28 +0800

    Add id to app setting table

--- database/migrations/2024_04_14_103039_create_app_settings_table.php
+++ database/migrations/2024_04_14_103039_create_app_settings_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('app_settings', function (Blueprint $table) {
-            // $table->id();
+            $table->id();
             $table->text('mission')->nullable();
             $table->text('vision')->nullable();
             $table->timestamps();
